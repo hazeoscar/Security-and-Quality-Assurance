02c5c3201d0778f4a9e4db7b517df588b7ab8f78
hazeoscar <32720437+hazeoscar@users.noreply.github.com>
2018-02-05 08:40:05 -0500

    HTML/JS files for lab 1 in HTTP 5201

---
+++ lab1.js
@@ -0,0 +1,27 @@
+/*jslint browser */
+
+
+var formIn = document.forms[0];
+var logIn = document.getElementById("login__btn");
+var displayName = document.getElementById("display__user");
+var displayPw = document.getElementById("display__pw");
+
+function submitGo() {
+    "use strict";
+    var userName = document.getElementById("login__user").value;
+    var passWord = document.getElementById("login__pw").value;
+    displayName.innerHTML = userName;
+    displayPw.innerHTML = passWord;
+    return false;
+}
+
+formIn.onsubmit = submitGo;
+
+
+
+
+
+
+
+
+
